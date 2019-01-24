847b6452e3607aa60f850bc34f8946ec4eb59238
Joe Ganobsik <jganobsik@gmail.com>
2019-01-24 17:33:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+
+    erb :team
+  end
 
 end
